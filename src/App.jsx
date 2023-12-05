dd65cdfeeb56d41516032135cd1a1a2c7d3ef32a
Ritu <singhritu241101@gmail.com>
2023-12-05 13:46:42 +0530

    Add post functionality initiated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./styles.css";
 import Sidebar from "./Sidebar";
-import Details from "./proCard";
 import Home from "./Home";
 
 function App() {
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Details from "./proCard";
+import Dailog from "./Dailog";
+import { Button, Modal, Space } from "antd";
 
 function Home() {
+  const [post, setpost] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const newPost = () => {
+    // setIsOpen(true);
+    Modal.confirm({
+      title: "Confirm",
+      content: "Bla bla ...",
+      footer: (_, { OkBtn, CancelBtn }) => (
+        <>
+          <Button>Custom Button</Button>
+          <CancelBtn />
+          <OkBtn />
+        </>
+      ),
+    });
+  };
   return (
-    <div>
-      <Details />
+    <div className="home-details">
+      <Button className="add-post-btn" onClick={newPost}>
+        +
+      </Button>
+      {/* {isOpen && <Dailog />} */}
     </div>
   );
 }
